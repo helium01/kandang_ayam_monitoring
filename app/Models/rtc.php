9735dac3356be62a72c4a4a7eb2a77b5a804695b
helium01 <fajarw935@gmail.com>
2023-10-30 15:07:35 +0700

    memulai projek

---
+++ app/Models/rtc.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class rtc extends Model
+{
+    use HasFactory;
+}
